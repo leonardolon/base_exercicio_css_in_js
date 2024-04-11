c6b6a05a86e42ad35d9a84ad61ccf8c0098e7954
leonardo <leonardo.lon@outlook.com>
2024-04-11 18:41:17 -0300

    styled components

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,5 +1,4 @@
-import styles from './Vaga.module.css'
-
+import { Lista, Titulo, VagaLink } from './styles'
 type Props = {
   titulo: string
   localizacao: string
@@ -11,8 +10,8 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <Lista>
+    <Titulo>{props.titulo}</Titulo>
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +21,8 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
-      Ver detalhes e candidatar-se
-    </a>
-  </li>
+    <VagaLink href="#">Ver detalhes e candidatar-se</VagaLink>
+  </Lista>
 )
 
 export default Vaga
--- src/components/Vaga/styles.ts
+++ src/components/Vaga/styles.ts
@@ -1,18 +1,30 @@
-.vaga {
+import styled from 'styled-components'
+
+export const Lista = styled.li`
   border: 1px solid var(--cor-principal);
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
   padding: 16px;
   transition: all ease 0.3s;
   border-radius: 8px;
-}
 
-.vagaTitulo {
+  &:hover {
+    background-color: var(--cor-principal);
+    color: var(--cor-secundaria);
+  }
+
+  &:hover a {
+    border-color: var(--cor-principal);
+    background-color: var(--cor-secundaria);
+    color: var(--cor-principal);
+  }
+`
+export const Titulo = styled.h3`
   font-weight: bold;
   margin-bottom: 16px;
-}
+`
 
-.vagaLink {
+export const VagaLink = styled.a`
   border-color: var(--cor-secundaria);
   background-color: var(--cor-principal);
   color: var(--cor-secundaria);
@@ -28,15 +40,4 @@
   @media (max-width: 768px) {
     display: block;
   }
-}
-
-.vaga:hover {
-  background-color: var(--cor-principal);
-  color: var(--cor-secundaria);
-}
-
-.vaga:hover a {
-  border-color: var(--cor-principal);
-  background-color: var(--cor-secundaria);
-  color: var(--cor-principal);
-}
+`
